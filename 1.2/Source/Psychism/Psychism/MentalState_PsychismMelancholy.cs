5db6605b355e1b3d48c292d86ac97eeed29554e6
dogproblems <thom@ashdigital.tech>
2021-03-07 21:44:59 +0000

    Proximity mood debuff effect

---
+++ 1.2/Source/Psychism/Psychism/MentalState_PsychismMelancholy.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace Psychism
+{
+    class MentalState_PsychismMelancholy : MentalState
+    {
+        public override void MentalStateTick()
+        {
+            base.MentalStateTick();
+
+            if (Gen.IsHashIntervalTick(pawn, 150))
+                return;
+
+            if (pawn.needs == null || pawn.needs.mood == null || pawn.Faction == null)
+                return;
+
+            if (pawn.Spawned)
+            {
+                this.AffectPawns(pawn, pawn.Map.mapPawns.AllPawnsSpawned);
+                return;
+            }
+            Caravan caravan = pawn.GetCaravan();
+            if (caravan != null)
+            {
+                this.AffectPawns(pawn, caravan.pawns.InnerListForReading);
+            }
+        }
+
+        private void AffectPawns(Pawn p, List<Pawn> pawns)
+        {
+            Hediff_ImplantWithLevel psylink = p.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.PsychicAmplifier) as Hediff_ImplantWithLevel;
+            if(psylink is null)
+                return;
+
+            for (int i = 0; i < pawns.Count; i++)
+            {
+                Pawn pawn = pawns[i];
+                if (p != pawn && 
+                    p.RaceProps.Humanlike && 
+                    pawn.needs != null && 
+                    pawn.needs.mood != null && 
+                    pawn.needs.mood.thoughts != null && 
+                    (!p.Spawned || !pawn.Spawned || pawn.Position.DistanceTo(p.Position) <= 9))
+                {
+                    bool flag = false;
+                    using (List<Thought_Memory>.Enumerator enumerator = pawn.needs.mood.thoughts.memories.Memories.GetEnumerator())
+                    {
+                        while (enumerator.MoveNext())
+                        {
+                            Thought_PsychicMelancholy oldThought = enumerator.Current as Thought_PsychicMelancholy;
+                            if (oldThought != null && oldThought.psylink.pawn == p)
+                            {
+                                flag = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!flag)
+                    {
+                        Thought_PsychicMelancholy thought = (Thought_PsychicMelancholy)ThoughtMaker.MakeThought(ThoughtDef.Named("PsychicMelancholy"));
+                        thought.psylink = p.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.PsychicAmplifier) as Hediff_ImplantWithLevel;
+                        pawn.needs.mood.thoughts.memories.TryGainMemory(thought, null);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ 1.2/Source/Psychism/Psychism/Thought_PsychicMelancholy.cs
@@ -0,0 +1,66 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace Psychism
+{
+    class Thought_PsychicMelancholy : Thought_Memory
+    {
+        public Hediff_ImplantWithLevel psylink;
+
+        public override string LabelCap
+        {
+            get
+            {
+                return base.CurStage.label.Formatted(this.psylink.pawn.Named("PSYCHISMSOURCE")).CapitalizeFirst();
+            }
+        }
+
+        public override bool ShouldDiscard
+        {
+            get
+            {
+                Pawn pawn = this.psylink.pawn;
+                return pawn.health.Dead || 
+                        pawn.needs == null || 
+                        pawn.needs.mood == null || 
+                        ((pawn.Spawned || this.pawn.Spawned || pawn.GetCaravan() != this.pawn.GetCaravan()) && (!pawn.Spawned || !this.pawn.Spawned || pawn.Map != this.pawn.Map || pawn.Position.DistanceTo(this.pawn.Position) > 9));
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_References.Look<Hediff_ImplantWithLevel>(ref this.psylink, "psylink", false);
+        }
+
+        public override float MoodOffset()
+        {
+            if (ThoughtUtility.ThoughtNullified(this.pawn, this.def))
+            {
+                return 0f;
+            }
+            if (ShouldDiscard)
+            {
+                return 0f;
+            }
+            float b = base.MoodOffset();
+            float ps = psylink.pawn.GetStatValue(StatDefOf.PsychicSensitivity, true);
+            float pl = psylink.level;
+            return b * ps * pl;
+        }
+
+        public override bool TryMergeWithExistingMemory(out bool showBubble)
+        {
+            showBubble = false;
+            return false;
+        }
+
+        public override bool GroupsWith(Thought other)
+        {
+            Thought_PsychicMelancholy thought = other as Thought_PsychicMelancholy;
+            return thought != null && base.GroupsWith(other) && thought.psylink == this.psylink;
+        }
+
+    }
+}
