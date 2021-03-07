45b9bc3b02d692c58afcc963311acc313ab79a7d
dogproblems <thom@ashdigital.tech>
2021-03-07 18:19:15 +0000

    Added dll, limits psychism to pawns with psilink

---
+++ 1.2/Source/Psychism/Psychism/MentalBreakWorker_Psychism.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace Psychism
+{
+    public class MentalBreakWorker_Psychism : MentalBreakWorker
+    {
+        public override bool BreakCanOccur(Pawn pawn)
+        {
+            if (!pawn.HasPsylink) return false;
+            return base.BreakCanOccur(pawn);
+        }
+    }
+} 
